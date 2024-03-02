c48ad41d7a6788ade7ff75cbb22ad1875b93910c
Mogamat-Muneeb <davimuneeb785@gmail.com>
2024-03-02 18:51:59 +0200

    wip

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,21 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import { connect } from "mongoose";
+import cors from "cors";
 const app = express();
+
+dotenv.config();
+
+connect(process.env.MONGO_URL || "")
+  .then(() => {
+    console.log("Mongo Connection successfully established ✅");
+  })
+  .catch((error: any) => {
+    console.error("Error connecting to Mongoose 👀", error);
+    process.exit(1);
+  });
+
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => res.send("FFx Backend"));
 
